64b81e1177446b679e2b159356db0e09e9941067
KaruzoDEV <35693714+KaruzoDEV@users.noreply.github.com>
2019-12-19 01:01:24 +0100

    A lot of stuff

--- src/main/java/com/mattymatty/RegionalPathfinder/LocationPair.java
+++ src/main/java/com/mattymatty/RegionalPathfinder/LocationPair.java
@@ -21,9 +21,13 @@ public class LocationPair {
     public Location getMaxCorner() { return maxCorner; }
 
     public boolean containsLocation(Location location) {
-        if(location.getBlockY() >= minCorner.getBlockY() && location.getBlockX() <= maxCorner.getBlockY()) {
-            if(location.getBlockX() >= minCorner.getBlockX() && location.getBlockX() <= maxCorner.getBlockX()) {
-                if(location.getBlockZ() >= minCorner.getBlockZ() && location.getBlockZ() <= maxCorner.getBlockZ()) {
+        return containsLocation(location,minCorner,maxCorner);
+    }
+
+    private static boolean containsLocation(Location checkLocation, Location minCorner, Location maxCorner) {
+        if(checkLocation.getBlockY() >= minCorner.getBlockY() && checkLocation.getBlockY() <= maxCorner.getBlockY()) {
+            if(checkLocation.getBlockX() >= minCorner.getBlockX() && checkLocation.getBlockX() <= maxCorner.getBlockX()) {
+                if(checkLocation.getBlockZ() >= minCorner.getBlockZ() && checkLocation.getBlockZ() <= maxCorner.getBlockZ()) {
                     return true;
                 }
             }
@@ -37,7 +41,7 @@ public class LocationPair {
         for (double x = minCorner.getBlockX(); x <= maxCorner.getBlockX(); x++) {
             for (double y = minCorner.getBlockY(); y <= maxCorner.getBlockY(); y++) {
                 for (double z = minCorner.getBlockZ(); z <= maxCorner.getBlockZ(); z++) {
-                    locationList.add(new Location(minCorner.getWorld(),x,y,z));
+                    locationList.add(new Location(minCorner.getWorld(),x+0.5,y,z+0.5));
                 }
             }
         }
@@ -45,8 +49,8 @@ public class LocationPair {
     }
 
     public static LocationPair getLocationPair(Location loc, List<Location> locList, Set<Location> ogLocationList) {
-        Location maxCorner = getCornerOfPair(loc,true,ogLocationList);
-        Location minCorner = getCornerOfPair(loc,false,ogLocationList);
+        Location maxCorner = getCornerOfPair(loc,true,ogLocationList,locList,null);
+        Location minCorner = getCornerOfPair(loc,false,ogLocationList,locList,maxCorner);
         LocationPair locationPair = new LocationPair(minCorner,maxCorner);
         for(Location location : new ArrayList<>(locList)) {
             if(location.getBlockY() == minCorner.getBlockY()) {
@@ -60,7 +64,7 @@ public class LocationPair {
         return locationPair;
     }
 
-    public static Location getCornerOfPair(Location loc, boolean positive, Set<Location> ogLocationList) {
+    public static Location getCornerOfPair(Location loc, boolean positive, Set<Location> ogLocationList, List<Location> changingList, Location setLocation) {
         Location corner = null;
         Location currentLoc = loc.clone();
         boolean keepX = true;
@@ -73,14 +77,14 @@ public class LocationPair {
         while(corner == null) {
             if(keepX) {
                 Location newLoc = currentLoc.clone().add(xVector);
-                if (ogLocationList.contains(newLoc)) {
+                if ((setLocation == null && containsEverything(loc,newLoc,ogLocationList,changingList)) || (setLocation != null && containsEverything(setLocation,newLoc,ogLocationList,changingList))) {
                     currentLoc = newLoc;
                 } else {
                     keepX = false;
                 }
             } else {
                 Location newLoc = currentLoc.clone().add(zVector);
-                if (ogLocationList.contains(newLoc)) {
+                if ((setLocation == null && containsEverything(loc,newLoc,ogLocationList,changingList)) || (setLocation != null && containsEverything(setLocation,newLoc,ogLocationList,changingList))) {
                     currentLoc = newLoc;
                 } else {
                     corner = currentLoc;
@@ -88,5 +92,23 @@ public class LocationPair {
             }
         }
         return corner;
+    }
+
+    private static boolean containsEverything(Location ogLocation, Location changingLocation, Set<Location> ogLocationList, List<Location> changingList) {
+        // We check if the number of missing locations inside the region and the number of actual locations inside match
+        int validInside = 0;
+        int totalInside;
+        for(Location location : ogLocationList) {
+            if(containsLocation(location,ogLocation,changingLocation)) {
+                if(changingList.contains(location)) {
+                    validInside++;
+                } else {
+                    // If the og list contains it but the changing one doesn't, this place has already been covered.
+                    return false;
+                }
+            }
+        }
+        totalInside = (Math.abs(ogLocation.getBlockX()-changingLocation.getBlockX())+1) * (Math.abs(ogLocation.getBlockY()-changingLocation.getBlockY())+1) * (Math.abs(ogLocation.getBlockZ()-changingLocation.getBlockZ())+1);
+        return validInside == totalInside;
     }
 }
